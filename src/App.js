6d846801e86f2306cdba3cc7cca646b4fba1461b
kim jae hun <zg788990@gmail.com>
2020-10-28 18:59:43 +0900

    make banner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import './App.css';
-import Row from './Row';
-import requests from './requests';
+import React from "react";
+import "./App.css";
+import requests from "./requests";
+import Row from "./Row";
+import Banner from "./Banner";
 
 function App() {
-  console.log('top',requests.fetchTopRated)
+  console.log("top", requests.fetchTopRated);
   return (
-    <sdiv className="App">
-      <h1>Hey clever programmer! let's build Netflix clone -coding</h1>
+    <div className="App">
+      {/* {Nav} */}
+      {/* {Banner} */}
+      <Banner />
       <Row
-      isLargeRow={true} 
-      title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+        isLargeRow
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
 
-      <Row title="Trending Now" fetchUrl={requests.fetchtTrending}/>
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/> 
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/> 
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/> 
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentares}/> 
-    </sdiv>
+      <Row title="Trending Now" fetchUrl={requests.fetchtTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentares} />
+    </div>
   );
 }
 
